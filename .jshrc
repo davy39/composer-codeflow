b9ea205d48a9a6513be6987b6a03cd0a72ed624a
davy39 <davy@ik.me>
2025-11-28 17:30:30 +0100

    up

---
+++ .jshrc
@@ -0,0 +1,2 @@
+export PATH=$PATH:$PWD/scripts:$PWD/vendor/bin
+export COMPOSER_HOME=$PWD/.composer
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,4 +4,4 @@ install-php.js
 install-composer.js
 composer config --global cache-dir $PWD/.cache
 composer config --global repositories.local-proxy path "$PWD/composer-proxy"
-npm run composer global install
+composer global install
